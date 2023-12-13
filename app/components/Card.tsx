bb3e909020291e8267ab8c7b88d4ec273e3d6943
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-12-13 08:03:21 +0100

    made it responsive

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,26 +1,30 @@
-import React from "react"
-
 export default function Card({
   title,
   text,
   imgSrc,
   color,
+  className = '',
+  ...otherProps
 }: {
   title: string
   text: string
   imgSrc: string
   color: string
+  className: string
 }) {
   return (
-    <div
-      className="border-t-[0.222rem] shadow-[0_15px_30px_-11px_rgba(131,166,210,0.50)] w-[19.4375rem] rounded-lg bg-white px-7 py-[1.69rem] text-left flex flex-col items-start mb-[1.56rem]"
-      style={{ borderColor: color }}
-    >
-      <h1 className="text-xl font-bold text-very-dark-blue">{title}</h1>
-      <p className="text-[0.8125rem] leading-[1.4375rem] tracking-[0.00563rem] text-very-dark-blue opacity-50 mb-[2.06rem]">
-        {text}
-      </p>
-      <img src={imgSrc} alt={`${title} icon`} className="self-end" />
+    <div className={`${className} flex h-full items-center justify-center`}>
+      <div
+        className="flex w-[19.4375rem] flex-col items-start rounded-lg border-t-[0.222rem] bg-white px-7 py-[1.69rem] text-left shadow-[0_15px_30px_-11px_rgba(131,166,210,0.50)]"
+        {...otherProps}
+        style={{ borderColor: color }}
+      >
+        <h1 className="text-xl font-bold text-very-dark-blue">{title}</h1>
+        <p className="mb-[2.06rem] text-[0.8125rem] leading-[1.4375rem] tracking-[0.00563rem] text-very-dark-blue opacity-50">
+          {text}
+        </p>
+        <img src={imgSrc} alt={`${title} icon`} className="self-end" />
+      </div>
     </div>
   )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,57 +1,54 @@
-import type { MetaFunction } from "@remix-run/node"
-import Card from "~/components/Card"
+import type { MetaFunction } from '@remix-run/node'
+import Card from '~/components/Card'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Four Card Feature Section" },
-    { name: "description", content: "A Challenge from Fronendmentor" },
-  ]
+  return [{ title: 'Four Card Feature Section' }, { name: 'description', content: 'A Challenge from Fronendmentor' }]
 }
-
-const cards = [
-  {
-    title: "Supervisor",
-    text: "Monitors activity to identify project roadblocks",
-    imgSrc: "/images/icon-supervisor.svg",
-    color: "#44D3D2",
-  },
-  {
-    title: "Team Builder",
-    text: "Scans our talent network to create the optimal team for your project",
-    imgSrc: "/images/icon-team-builder.svg",
-    color: "#EA5454",
-  },
-  {
-    title: "Karma",
-    text: "Regularly evaluates our talent to ensure quality",
-    imgSrc: "/images/icon-karma.svg",
-    color: "#FCAE4A",
-  },
-  {
-    title: "Calculator",
-    text: "Uses data from past projects to provide better delivery estimates",
-    imgSrc: "/images/icon-calculator.svg",
-    color: "#549EF2",
-  },
-]
 
 export default function Index() {
   return (
-    <main className="bg-very-light-gray min-h-screen flex items-center justify-center px-10 py-20 text-center">
+    <main className=" grid min-h-screen place-items-center bg-very-light-gray px-10 py-20 text-center">
       <div className="flex flex-col items-center">
-        <h1 className="text-2xl font-extralight tracking-[0.1044rem] text-very-dark-blue">
+        <h1 className="text-2xl font-extralight tracking-[0.1044rem] text-very-dark-blue lg:text-4xl lg:tracking-[0.01563]">
           Reliable, efficient delivery
         </h1>
-        <h2 className="text-2xl font-semibold tracking-[0.1044rem] text-very-dark-blue mb-[0.81rem]">
+        <h2 className="mb-[0.81rem] text-2xl font-semibold tracking-[0.1044rem] text-very-dark-blue lg:text-4xl lg:tracking-[0.01563]">
           Powered by Technology
         </h2>
-        <p className="text-[0.9375rem] leading-[1.5625rem] tracking-[0.0065rem] text-very-dark-blue mb-[4.75rem] opacity-50">
-          Our Artificial Intelligence powered tools use millions of project data
-          points to ensure that your project is successful
+        <p className="mb-[4.75rem] text-[0.9375rem] leading-[1.5625rem] tracking-[0.0065rem] text-very-dark-blue opacity-50">
+          Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is
+          successful
         </p>
-        {cards.map((card) => (
-          <Card key={card.title} {...card} />
-        ))}
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+          <Card
+            title="Supervisor"
+            text="Monitors activity to identify project roadblocks"
+            imgSrc="/images/icon-supervisor.svg"
+            color="#44D3D2"
+            className="lg:row-span-2"
+          />
+          <Card
+            title="Team Builder"
+            text="Scans our talent network to create the optimal team for your project"
+            imgSrc="/images/icon-karma.svg"
+            color="#EA5454"
+            className=""
+          />
+          <Card
+            title="Karma"
+            text="Regularly evaluates our talent to ensure quality"
+            imgSrc="/images/icon-team-builder.svg"
+            color="#FCAE4A"
+            className=""
+          />
+          <Card
+            title="Calculator"
+            text="Uses data from past projects to provide better delivery estimates"
+            imgSrc="/images/icon-calculator.svg"
+            color="#549EF2"
+            className="lg:col-start-3 lg:row-span-2 lg:row-start-1"
+          />
+        </div>
       </div>
     </main>
   )
